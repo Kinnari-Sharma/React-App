70c47b1ac434e1fccfe39b809c9afd5dd305f03c
Kinnary Sharma <kinnarysharma@localhost.localdomain>
2018-07-06 22:53:00 +0530

    Implementing datepicker

--- src/components/DateFilter.js
+++ src/components/DateFilter.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+// import DatePicker from 'react-datepicker';
+import { SingleDatePicker } from 'react-dates';
+// import SingleDatePickerWrapper from '../examples/SingleDatePickerWrapper';
+import 'react-dates/lib/css/_datepicker.css';
+import 'react-dates/initialize';
+// import 'react-datepicker/dist/react-datepicker.css';
 import { format_date } from '../helpers/format_date';
 
 export default class DateFilter extends React.Component {
@@ -8,7 +12,8 @@ export default class DateFilter extends React.Component {
 	constructor (props) {
     super(props);
     this.state = {
-      startDate: null
+      startDate: null,
+      focused: null
     };
   }
  
@@ -21,15 +26,31 @@ export default class DateFilter extends React.Component {
 
 	render() {
 		return(
-			<DatePicker
-		  	selected={this.state.startDate}
-	  		onChange={this.handleChange}
-	  		dateFormat="DD/MM/YYYY"
-	  		isClearable={true}
-	  		placeholderText="Date Filter"
-	  		className = "date-filter"
-        clearButtonTitle = "Clear"
-        ref={datePicker => {this.datePicker = datePicker}}
+			// <DatePicker
+		 //  	selected={this.state.startDate}
+	  // 		onChange={this.handleChange}
+	  // 		dateFormat="DD/MM/YYYY"
+	  // 		isClearable={true}
+	  		// placeholderText="Date Filter"
+	  		// className = "date-filter"
+   //      clearButtonTitle = "Clear"
+   //      ref={datePicker => {this.datePicker = datePicker}}
+			// />
+			
+        <SingleDatePicker
+        	// selected={this.state.startDate}
+          showClearDate={true}
+          small={true}
+          block={false}
+          numberOfMonths={1}
+          // placeholderText="Date Filter"
+	  			// className = "date-filter"
+          onDateChange={this.handleChange}
+          focused={this.state.focused}
+          onFocusChange={({ focused }) =>
+            this.setState({ focused })
+          }
+          hideKeyboardShortcutsPanel={true}
 			/>
 		);
 	}
